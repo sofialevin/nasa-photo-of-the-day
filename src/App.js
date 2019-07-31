03a864ccd866b587a014cd644ec43af69feed015
sofialevin <slevin@M-4876.local>
2019-07-30 19:30:00 -0700

    Imported axios, added useEffect and completed sample API call.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 import ImageDetails from "./components/ImgDetails/ImageDetails";
 import Image from "./components/Image";
 
 function App() {
+  const [image, setImage] = useState();
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(result => console.log(result))
+  }, [])
+
   return (
     <div className="App">
       <Image />
